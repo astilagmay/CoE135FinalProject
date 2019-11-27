2d81f3348f1b70793dea69ce0dd5fc72b73d5078
Asti <lagmayarnold@gmail.com>
2019-11-27 14:58:41 +0800

    Finished half of menu

--- main.py
+++ main.py
@@ -34,4 +34,65 @@ if __name__ == '__main__':
     bsocket.sendto('kold'.encode(), (ip, port))
     p.join()
 
+            #except:
+            bsocket.settimeout(None)
+            print("[MAIN] No IPs found")
+            p.terminate()
+
+            break
+
+        #view contacts
+        elif (option == 3):
+            break
+
+        #add contact
+        elif (option == 4):
+            break
+
+        #change settings
+        elif (option == 5):
+
+            if (setting == 0):
+                print("\nReceiving from Everyone by default\n")
+
+            elif (setting == 1):
+                print("\nReceiving is Off\n")
+
+            elif (setting == 2):
+                print("\nReceiving from Contacts Only\n")
+
+            else:
+                print("\nReceiving from Everyone\n")
+
+            while(True):
+
+                setting = int(input("New setting: "))
+
+                if (setting == 1):
+                    print("\nReceiving is now Off\n")
+                    break
+
+                elif (setting == 2):
+                    print("\nNow receiving from Contacts Only\n")
+                    break
+
+                elif (setting == 3):
+                    print("\nNow receiving from Everyone\n")
+                    break
+
+                else:
+                    print("Invalid setting")
+            
+            continue
+            break
+
+        #exit
+        elif (option == 6):
+            print("\nExiting program")
+            exit()
+
+        #default
+        else:
+            print("Invalid command")
+
 
