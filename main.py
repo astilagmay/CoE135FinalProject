45f9c4f6bc1133bb55b6549f8f02cdbdda281b09
Asti <lagmayarnold@gmail.com>
2019-12-08 03:33:02 +0800

    Listener done and menu updates

--- main.py
+++ main.py
@@ -1,62 +1,24 @@
 from socket import *
-from multiprocessing import Process, Pipe
+from multiprocessing import Process, Queue
 import os
 import sys
 
-def checker(data):
-
-    timeout = 1
-    timeout_count = 0
-    
-    print("[CHECKER] ", end="")
-    print(data)
-
-    # Create a TCP/IP socket
-    tcp_sock = socket(AF_INET, SOCK_STREAM)
-    server_address = (data, 8000)
-    print('connecting to {} port {}'.format(*server_address))
-    tcp_sock.connect(server_address)
-
-    tcp_sock.settimeout(timeout)
-
-    while True:
-        try:
-            tcp_sock.send("[CHECKER] TCP")
-
-        except:
-            timeout_count = timeout_count + 1
-
-        finally:
-            print("[CHECKER] closing " + str(timeout_count))
-            if timeout_count == 10:
-                break
-        
-    tcp_sock.close()
-
-
 def listener(pipe):
-    #set timeout
-    timeout = 1
-    timeout_count = 0
-    pi_list = []
+    
+    #set variables
+    # timeout_count = 0
+    # timeout_limit = 5
+    ip_list = []
+    port = 8080
 
     #bind to local address
     udp_sock = socket(AF_INET, SOCK_DGRAM)
-    udp_sock.bind(('' , 8080))
+    udp_sock.bind(('', port))
 
     #receive from broadcast
     while True:
 
-        #check active processes
-        for i, pi in enumerate(pi_list):
-            if (pi_list[i][0].is_alive()):
-                pass
-            else:
-                pi_list.remove((pi_list[i][0], pi_list[i][1]))
-
-        #set timeout
-        udp_sock.settimeout(timeout)
-
+        #recieve broadcast packet
         try:
             data, address = udp_sock.recvfrom(64)
             #print data
@@ -66,24 +28,59 @@ def listener(pipe):
             # create
             if (data.decode() != None):
                 timeout_count = 0
+                ip_list.append(address[0])
+                pipe.put(ip_list)
 
-                proc = Process(target = checker, args = (address[0],))
-                pi.append((proc,address[0]))
-                proc.start()
-
-            #send to main
-            pipe.send([data.decode(), address])
-
+        #nothing received
         except:
-            timeout_count = timeout_count + 1
+            continue
+            # timeout_count = timeout_count + 1
 
-        finally:
-            print("[LISTENER] closing " + str(timeout_count))
-            if timeout_count == 10:
-                print(pi_list)
-                break
+        # finally:
+        #     #print("[LISTENER] timeout " + str(timeout_count))
+        #     if timeout_count == timeout_limit:
+        #         #print("[LISTENER] closed")
+                
+        #         print("[LISTENER] closed, IP list: ", end="")
+        #         print(ip_list)
+                
+        #         pipe.put("Done")
+        #         #break
 
     pipe.close() 
+
+def check_iplist(q_listener):
+
+    #check if IPs are active
+    try:
+        data = q_listener.get(False)  
+
+        if (data != "Done"):
+            ip_list = data
+
+        for ip in ip_list:
+            tcp_sock = socket()
+            address = ip
+
+            try:
+                tcp_sock.connect((address, port)) 
+            
+            #ip is offline
+            except:
+                #print("%s:%d: Exception is %s" % (address, port, e))
+                #ip_list.remove(ip)
+                pass
+            
+            finally:
+                tcp_sock.close()  
+
+    #queue is empty
+    except:
+        ip_list = []
+        # print("\n[MAIN] IP list is empty\n")
+
+    return ip_list
+
 
 if __name__ == '__main__':
 
@@ -91,23 +88,18 @@ if __name__ == '__main__':
     timeout = 1
     setting = 0
     contacts = []
-    kill_thread = 0
+    ip_list = []
+    port = 8080
 
-    #initialize pipes
-    p_main, p_listener = Pipe()
-    p = Process(target = listener, args = (p_listener,))
+    #initialize queue and process
+    q_listener = Queue()
+    p = Process(target = listener, args = (q_listener,))
+    p.daemon = True
 
-    p.start()
+    #start listener
+    p.start() 
 
-    # receiver = p_main.recv()
-    # ip = receiver[1][0]
-    # port = receiver[1][1]
-    # print("[MAIN] ", end="")
-    # print(ip, port)
-
-    #menu loop
     while(True):
-
         # print menu options
         print("Choose a command:")
         print("(1) Send file")
@@ -116,28 +108,45 @@ if __name__ == '__main__':
         print("(4) Add contact")
         print("(5) Change AirDrop settings")
         print("(6) Exit")
-        option = input()
-        option = int(option)
+        option = int(input(": "))
 
         #send file - NOT DONE
         if (option == 1):
+            ip_list = check_iplist(q_listener)
 
-            break
+            if not ip_list:
+                ip_list = []
+                print("\nIP list is empty\n")
+
+            else:
+                print("\nSelect IP:")
+                for i, ip in enumerate(ip_list):
+                    print("(" + str(i) + ") " + ip)
+
+                print("")
+                ip_choice = int(input(": "))
+
+                if ip_choice > (len(ip_list) - 1):
+                    print("\nInvalid IP\n")
+                    continue
 
         #view network IPs - NOT DONE
         elif (option == 2):
+            ip_list = check_iplist(q_listener)
 
-            #start thread
-    
-            #send UDP broadcast
-            bsocket = socket(AF_INET, SOCK_DGRAM)
-            bsocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-            bsocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-            bsocket.sendto('UDP Broadcast'.encode(), ('255.255.255.255', 54545))
+            if not ip_list:
+                ip_list = []
+                print("\nIP list is empty\n")
 
-            bsocket.settimeout(timeout)
+            else:
+                print("\nIP list: ")
 
-            break
+                for ip in ip_list:
+                    print(ip)
+
+                print("")
+
+                #print("\n")
 
         #view contacts
         elif (option == 3):
@@ -166,6 +175,7 @@ if __name__ == '__main__':
 
                 print("\n")
 
+            #close file
             f.close()
 
         #add contact - NOT DONE
@@ -211,12 +221,20 @@ if __name__ == '__main__':
 
         #exit
         elif (option == 6):
-            print("\nExiting program")
+
+            print("\nClosing processes")
+            p.terminate()
             p.join()
+            print("Exiting program")
             exit()
 
         #default
         else:
             print("Invalid command")
 
+
+                
+
+
+    
 
--- sender.py
+++ sender.py
@@ -7,36 +7,36 @@ bsocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 bsocket.sendto('UDP Broadcast'.encode(), ('255.255.255.255', 8080))
 
 
-# Create a TCP/IP socket
-sock = socket(AF_INET, SOCK_STREAM)
+# # Create a TCP/IP socket
+# sock = socket(AF_INET, SOCK_STREAM)
 
-# Bind the socket to the port
-server_address = ('localhost', 10000)
-print('starting up on {} port {}'.format(*server_address))
-sock.bind(server_address)
+# # Bind the socket to the port
+# server_address = ('localhost', 10000)
+# print('starting up on {} port {}'.format(*server_address))
+# sock.bind(server_address)
 
-# Listen for incoming connections
-sock.listen(1)
+# # Listen for incoming connections
+# sock.listen(1)
 
-while True:
-    # Wait for a connection
-    print('waiting for a connection')
-    connection, client_address = sock.accept()
+# while True:
+#     # Wait for a connection
+#     print('waiting for a connection')
+#     connection, client_address = sock.accept()
     
-    try:
-        print('connection from', client_address)
+#     try:
+#         print('connection from', client_address)
 
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(16)
-            print('received {!r}'.format(data))
-            if data:
-                print('sending data back to the client')
-                connection.sendall(data)
-            else:
-                print('no data from', client_address)
-                break
+#         # Receive the data in small chunks and retransmit it
+#         while True:
+#             data = connection.recv(16)
+#             print('received {!r}'.format(data))
+#             if data:
+#                 print('sending data back to the client')
+#                 connection.sendall(data)
+#             else:
+#                 print('no data from', client_address)
+#                 break
 
-    finally:
-        # Clean up the connection
-        connection.close()
+#     finally:
+#         # Clean up the connection
+#         connection.close()
